418acceb5fcfb863fedebd4a388090d07058e31e
Thomsons Nagaba <tnagaba2012@gmail.com>
2026-07-16 18:38:06 +0300

    adding the minerals.py file to the commit

--- pages/minerals.py
+++ pages/minerals.py
@@ -1,14 +1,19 @@
-import streamlit as st
 
+import streamlit as st
+from chapters.chapter1 import show_chapter1
 def show_page():
 
     st.title("🧪 Minerals & Chemistry")
 
     st.info("Explore Uganda's mineral resources and industrial chemistry.")
+st.divider()
 
-    st.header("Topics")
+st.header("📚 Uganda Minerals Master Handbook")
 
-    st.markdown("""
+show_chapter1()
+  
+
+st.markdown("""
 - 🟤 Iron Oxide Pigments
 - ⚪ Kaolin
 - 🟢 Bentonite
